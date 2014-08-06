983872868b48df8002f930eac1a90b063f8317d2
Yuuki Takano <ytakanoster@gmail.com>
2014-08-06 12:47:08 +0900

    change Eclipse License to 3-Clause License

--- http/project.clj
+++ http/project.clj
@@ -1,9 +1,10 @@
-(defproject http "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject http "0.1.0-stap-http"
+  :description "HTTP Parser for STAP"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :license {:name "3-Clause BSD License"
+            :url "http://opensource.org/licenses/BSD-3-Clause"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [local/junixsocket "1.3"]]
   :main ^:skip-aot http.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- http/src/http/core.clj
+++ http/src/http/core.clj
@@ -1,7 +1,9 @@
 (ns http.core
-  (:gen-class))
+  (:gen-class)
+  (:import [org.newsclub.net.unix AFUNIXServerSocket]))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (let [uxpath (nth args 0 "/tmp/stap/tcp/http")]
+    (println uxpath)))
